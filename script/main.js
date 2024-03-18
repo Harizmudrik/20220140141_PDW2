fa010abbd8bce753f8cff9c245956e5756053980
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-03-19 00:02:23 +0700

    mantap

---
+++ script/main.js
@@ -0,0 +1,15 @@
+const waktu = document.getElementById("waktu");
+
+function tanggal() {
+    const date = new Date();
+    waktu.innerHTML = date;
+    
+}
+
+setInterval(tanggal, 1000);
+
+tanggal();
+
+
+
+
